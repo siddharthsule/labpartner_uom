b6abf3c3770631d445e16ab646ce470308abfa81
Siddharth Sule <siddharthsule@outlook.com>
2024-01-01 16:11:31 +0000

    change python versions for test

--- setup.py
+++ setup.py
@@ -30,16 +30,15 @@ setuptools.setup(
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     # install_requires=['Pillow'],
     extras_require={
         'dev': ['check-manifest'],
