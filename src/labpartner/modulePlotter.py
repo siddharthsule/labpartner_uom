2dcc626e41acafe723843e98565e6590c18c0bc0
Siddharth Sule <siddharthsule@outlook.com>
2024-01-01 18:42:11 +0000

    Add plotting functionality

---
+++ src/labpartner/modulePlotter.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+from .moduleLSFR import *
+
+
+class plotter:
+
+    @staticmethod
+    def plot(x, y, yerr, xlabel="x axis", ylabel="y axis", title=None, label="data", fit=None, figsize=(4, 3)):
+
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+        ax.errorbar(x, y, yerr=yerr, fmt='.', capsize=4, label=label)
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        ax.set_title(title) if title is not None else None
+
+        if fit == "lin":
+
+            fit = linfit.do_linear_fit(x, y, yerr)
+            ax.plot(x, fit[0]*x + fit[2], label='Linear Fit')
+
+        elif fit == "quad":
+
+            fit = linfit.do_quadratic_fit(x, y, yerr)
+            ax.plot(x, fit[0]*x**2 + fit[2]*x + fit[4], label='Quadratic Fit')
+
+        ax.legend(loc='best')
+
+        fig.tight_layout()
+        fig.savefig('myplot.png', dpi=300)
+        fig.savefig('myplot.pdf')
+
+    @staticmethod
+    def print_help():
+
+        startup_info = """
+        Plotting Tool
+        -------------
+        This software uses the Matplotlib module to plot a set of data points.
+
+        Usage
+        -----
+        To use this software, run the following command:
+        import labpartner as lp
+        lp.plotter.plot(x, y, yerr=None, xlabel="x axis", ylabel="y axis", title=None, label="data", fit=None, figsize=(4, 3))
+
+        Example
+        -------
+        import labpartner as lp
+        lp.plotter.plot([1, 2, 3], [4, 5, 6])
+
+        Returns
+        -------
+        A plot of the data points.
+        """
+
+        print(startup_info)
---
+++ tests/test_modulePlotter.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+import os
+from labpartner.modulePlotter import plotter
+import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import check_figures_equal
+
+class TestPlotter(unittest.TestCase):
+
+    def test_plot_creates_file(self):
+        x = np.array([1, 2, 3])
+        y = np.array([2, 4, 6])
+        yerr = np.array([0.1, 0.2, 0.3])
+
+        # Call the plot method
+        plotter.plot(x, y, yerr, fit="lin")
+
+        # Check if the file was created
+        self.assertTrue(os.path.exists('myplot.png'))
+        self.assertTrue(os.path.exists('myplot.pdf'))
+
+        # Clean up the created files
+        os.remove('myplot.png')
+        os.remove('myplot.pdf')
+
+if __name__ == '__main__':
+    unittest.main()
